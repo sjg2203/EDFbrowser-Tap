afae86cbef21c94eeedcabd127b0845921c69916
CI Auto Tagger <ci@example.com>
2026-01-23 16:27:54 +0000

    Created cask for EDFbrowser

---
+++ Casks/edfbrowser.rb
@@ -0,0 +1,30 @@
+cask "edfbrowser" do
+  version "2.14"
+  sha256 "1b5651c6f09dd3186e9c3007466ac9d66b4066eff137ea76cb6b7e37fba072f8"
+
+  url "https://gitlab.com/sjg2203/edfbrowser-silicon/-/jobs/artifacts/v#{version}/raw/build_output/EDFbrowser-v#{version}.dmg?job=package_dmg"
+  name "EDFbrowser"
+  desc "EDF+ and BDF+ viewer and toolbox – Apple Silicon build"
+  homepage "https://gitlab.com/sjg2203/edfbrowser-silicon"
+
+  livecheck do
+    url "https://gitlab.com/api/v4/projects/72163637/repository/tags"
+    strategy :json do |json|
+      json.map do |tag|
+        next unless tag["name"]
+
+        tag["name"][/^v?(\d+(?:\.\d+)+)$/i, 1]
+      end
+    end
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :ventura"
+
+  app "EDFbrowser.app"
+
+  zap trash: [
+    "~/.EDFbrowser",
+    "~/Library/Saved Application State/net.teuniz.EDFbrowser.savedState",
+  ]
+end
