37cd26bedfed7a806b7411feceac28a090dc801e
CI Homebrew Updater <noreply@ciupdater.com>
2026-05-26 20:24:09 +0100

    Update EDFbrowser to 2.14
    SHA256: 9945e686cb0ef537df86a06841352ea7f4cadf2eab8c6d13f9d27090e4ebb107

--- Casks/edfbrowser.rb
+++ Casks/edfbrowser.rb
@@ -1,6 +1,6 @@
 cask "edfbrowser" do
   version "2.14"
-  sha256 "d775de567fb2e492762f4d15c378b4217e1e80283b7ada31f53cab6c90fbed12"
+  sha256 "9945e686cb0ef537df86a06841352ea7f4cadf2eab8c6d13f9d27090e4ebb107"
 
   url "https://gitlab.com/sjg2203/edfbrowser-silicon/-/jobs/artifacts/v#{version}/raw/build_output/EDFbrowser-v#{version}.dmg?job=package_dmg"
   name "EDFbrowser"
